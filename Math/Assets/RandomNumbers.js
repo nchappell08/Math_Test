2f6e1d376d0063441e516e714ef2b4c58e62189e
Nick Chappell <chappellscastle@gmail.com>
2015-02-01 17:49:07 -0800

    Unity Project

---
+++ Math/Assets/RandomNumbers.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+public var UIAnswer:UI.Text;
+public var UINumber1:UI.Text;
+public var UINumber2:UI.Text;
+public var UIAnswer2:UI.Text;
+public var UIAnswer3:UI.Text;
+
+private var number1:int;
+private var number2:int;
+private var answer1:int;
+private var answer2:int;
+private var answer3:int;
+
+function Start ()
+{
+	number1 = Random.Range( 3.0, 9.0 );
+	number2 = Random.Range( 3.0, 9.0 );
+	
+	answer1 = number1 + number2;
+	answer2 = Random.Range( 6.0, 18.0 );
+	answer3 = Random.Range( 6.0, 18.0 );
+	
+	UIAnswer.text = answer1.ToString();
+	UIAnswer2.text = answer2.ToString();
+	UIAnswer3.text = answer3.ToString();
+	UINumber1.text = number1.ToString();
+	UINumber2.text = number2.ToString();
+}
+
+function Update ()
+{
+	
+}
+
---
+++ Math/Assets/TestClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestClick : MonoBehaviour 
+{
+
+	public void ClickTest ()
+	{
+		Debug.Log ("Clicked");
+	}
+
+	public void ClickTest2 (string text)
+	{
+		Debug.Log (text);
+	}
+}
